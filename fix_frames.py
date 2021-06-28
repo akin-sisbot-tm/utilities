a4118cbb8157f9278a699c70a0479c47cd73288b
Akin Sisbot <akin.sisbot@toyota.com>
2021-06-28 10:34:29 -0700

    first commit

---
+++ fix_frames.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3 
+
+
+import rosbag
+import sys
+
+newfile = sys.argv[1].replace(".bag","")+"_fixed.bag"
+
+with rosbag.Bag(newfile, 'w') as outbag:
+	for topic, msg, t in rosbag.Bag(sys.argv[1]).read_messages():
+		if msg._has_header:
+			if len(msg.header.frame_id) > 0:
+				if msg.header.frame_id[0] == '/':
+					msg.header.frame_id = msg.header.frame_id[1:]
+		
+		outbag.write(topic, msg, t)
+
+
+	outbag.close()
